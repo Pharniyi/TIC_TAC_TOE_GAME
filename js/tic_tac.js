1e9a8175d42404d2bd5dc62d754478735b4061ab
Pharniyi <99425154+Pharniyi@users.noreply.github.com>
2024-04-09 14:00:21 -0700

    Add files via upload

--- js/tic_tac.js
+++ js/tic_tac.js
@@ -10,3 +10,55 @@ let currentPlayer=X_TEXT
 let spaces=Array(9).fill(null)
 
 console.log(spaces)
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked (e) {
+    /*console.log(e.target)*/
+    const id = e.target.id
+    if(!spaces[id]){
+        spaces[id]= currentPlayer
+        e.target.innerText = currentPlayer
+
+        currentPlayer= currentPlayer== X_TEXT ? O_TEXT:X_TEXT
+
+        if(playerHasWon()){
+            playerText= `${currentPlayer} has won`
+        }
+    }
+}
+
+const winningcombo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [1,4,7],
+    [0,3,6],
+    [2,5,8],
+    
+]
+
+function playerHasWon(){
+    for (const condition of winningcombo) {
+        let [a,b,c] = condition
+    }
+}
+rstartBtn.addEventListener('click' , restart)
+
+function restart(){
+    spaces.fill(null)
+    boxes.forEach(box=>{
+        box.innerText=''
+    })
+
+    playerText='Tic Tac Toe'
+   currentPlayer=X_TEXT 
+
+}
+
+
+startGame()
